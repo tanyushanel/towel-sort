84c960140aaa16eeb00dab1a06bf27ebd02c17cb
tanyushanel <tanyushanel@users.noreply.github.com>
2020-03-20 18:37:53 +0300

    feat: solution added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (matrix && matrix != []) {
+        let arrSortOdd = matrix.map((i) => matrix.indexOf(i) % 2 ? i.sort((a, b) => b - a) : i);
+        let str = arrSortOdd.join(',');
+        let arrNotEmpty = str.split(',').filter((el) => el != "");
+        let arrNumb = arrNotEmpty.map((item) => +item);
+        return arrNumb;
+    } else return [];
 }
